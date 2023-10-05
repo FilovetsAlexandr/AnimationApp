5089d13adb3ed63fc24b72979eb5c1ccfae1f1b9
FilovetsAlexandr <sohan.boxan@gmail.com>
2023-10-05 11:03:23 +0300

    project done
    With lottie. Install cocoapods. Use terminal :skull:

---
+++ AnimationApp/AnimationApp/Models/AnimationsModels.swift
@@ -0,0 +1,35 @@
+//
+//  AnimationsModels.swift
+//  AnimationApp
+//
+//  Created by Alexandr Filovets on 5.10.23.
+//
+
+import Lottie
+
+struct AnimationsModels {
+    static let animationsImages: [LottieAnimationView] = [
+        LottieAnimationView(name: "3-d"),
+        LottieAnimationView(name: "bear"),
+        LottieAnimationView(name: "bee"),
+        LottieAnimationView(name: "cat-head"),
+        LottieAnimationView(name: "coral"),
+        LottieAnimationView(name: "duck"),
+        LottieAnimationView(name: "frysztak-town"),
+        LottieAnimationView(name: "home"),
+        LottieAnimationView(name: "honeycombs"),
+        LottieAnimationView(name: "horse"),
+        LottieAnimationView(name: "maneki-cat"),
+        LottieAnimationView(name: "paws-animal-morph"),
+        LottieAnimationView(name: "pets"),
+        LottieAnimationView(name: "savings-pig"),
+        LottieAnimationView(name: "shark"),
+        LottieAnimationView(name: "snake"),
+        LottieAnimationView(name: "sphere"),
+        LottieAnimationView(name: "spider"),
+        LottieAnimationView(name: "squirrel"),
+        LottieAnimationView(name: "turkey"),
+        LottieAnimationView(name: "wings"),
+        LottieAnimationView(name: "woodpecker"),
+    ]
+}
